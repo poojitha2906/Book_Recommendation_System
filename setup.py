4ada4b89850bec097e360b303982ec86d56852d1
Paltasingi Poojitha <paltasingi.poojitha.eee21@itbhu.ac.in>
2023-06-07 18:20:31 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+REPO_NAME = "Books-Recommender-System"
+AUTHOR_USER_NAME = "poojitha.2906"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for Movie Recommender System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email="poojithapaltasingi2906@gmail.com",
+    packages=[SRC_REPO],
+    python_requires=">=3.11.3",
+    install_requires=LIST_OF_REQUIREMENTS
+)
